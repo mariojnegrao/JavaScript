105bf704c9a163082b33c1538ba8d3835f0ebbcd
Mário Negrão de Barros <168686240+mariojnegrao@users.noreply.github.com>
2024-11-01 08:48:08 +0100

    Ex 17

--- aula14ex/ex017/meuscript.js
+++ aula14ex/ex017/meuscript.js
@@ -1,5 +1,21 @@
-function tabela(){
-    let n = document.getElementById("in1")
-    let t = document.getElementById("res")
-    
+function tabuada(){
+    let num = document.getElementById("in1")
+    let tab = document.getElementById("seltab")
+    if( tab.length != 0){
+        tab.length = 0
+    }
+    if( num.value.length == 0){
+        window.alert (`Por favor , Digite um numero`)
+    }
+    else{
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 15){
+            let item = document.createElement(`option`)
+            item.text = `${n} X ${c} = ${n * c}`
+            item.value = `tab ${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
 }
